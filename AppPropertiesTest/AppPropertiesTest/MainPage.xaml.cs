47d2e502e741afba907ea1884b962db2639afb27
Flavio Goncalves <f.goncalves@cayas.de>
2024-01-10 17:08:20 +0100

    Add repro

---
+++ AppPropertiesTest/AppPropertiesTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppPropertiesTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Application.Current.Properties.TryGetValue("test", out var value);
+
+            Application.Current.Properties["test"] = "123";
+            Application.Current.SavePropertiesAsync();
+        }
+    }
+}
---
+++ AppPropertiesTestMaui/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Plugin.Maui.FormsMigration;
+
+namespace AppPropertiesTestMaui;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        if (LegacyApplication.Current.Properties.ContainsKey("test"))
+        {
+            var value = (string)LegacyApplication.Current.Properties["test"];
+        }
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
+
